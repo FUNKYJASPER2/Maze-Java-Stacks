3775cb7db56774c84627766a8c5dd698b56659cb
Jasper Cools <js452275@dal.ca>
2018-04-13 11:47:48 -0300

    menus almost all done with customization

--- src/Maze.java
+++ src/Maze.java
@@ -1,15 +1,13 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
@@ -17,17 +15,19 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
-import java.util.Scanner;
+import javafx.scene.image.ImageView;
+
 
 import static javafx.scene.paint.Color.BLACK;
 import static javafx.scene.paint.Color.GRAY;
 import static javafx.scene.paint.Color.WHITE;
 
 public class Maze extends Application implements EventHandler<KeyEvent> {
-    RandomMaze maze = new RandomMaze(15);
-    private double startX = 500;
+    private int moveCount = 0;
+    private int size;
+    private RandomMaze maze = new RandomMaze(15);
+    private double startX = 775;
     private double startY = 300;
     private Stage window;
     private double playerXpos = startX+2;
@@ -35,6 +35,9 @@ public class Maze extends Application implements EventHandler<KeyEvent> {
     private int playRow = 0;
     private int playCol = 0;
     private Rectangle recPlayer = new Rectangle(playerXpos, playerYpos, 16, 16);
+    private Text moveCounterTitle = new Text(1675,300, ""+moveCount);
+    private Image menuTitle = new Image("menuname.gif");
+    private ImageView title = new ImageView(menuTitle);
 
 
 
@@ -54,26 +57,21 @@ public class Maze extends Application implements EventHandler<KeyEvent> {
         Group root;
         root = new Group();
 
-
-        Text title = new Text(900,100,"Maze Game");
-        title.setFont(Font.font("Arial", FontWeight.BOLD, FontPosture.ITALIC, 80));
-        title.setFill(WHITE);
-        title.setX(725);
-        title.setY(100);
+        title.setX(525);
 
         // scene is created
-        Scene mainMenuScene = new Scene(root, 1920, 1080, Color.GRAY);
+        Scene mainMenuScene = new Scene(root, 1920, 1080, Color.web("#272932"));
+        mainMenuScene.getStylesheets().add("Styles.css");
 
         Button playButton = new Button("Play");
         playButton.setTranslateX(750);
         playButton.setTranslateY(300);
         playButton.setMinSize(400,100);
-        playButton.setStyle("-fx-font-size: 4em; ");
         // event handler for when its clicked and changes the color
         playButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                window.setScene(playGameScene());
+                window.setScene(askSizeScene());
             }
         });
 
@@ -81,7 +79,6 @@ public class Maze extends Application implements EventHandler<KeyEvent> {
         scoreButton.setTranslateX(750);
         scoreButton.setTranslateY(500);
         scoreButton.setMinSize(400,100);
-        scoreButton.setStyle("-fx-font-size: 4em; ");
         // event handler for when its clicked and changes the color
         scoreButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
@@ -94,7 +91,6 @@ public class Maze extends Application implements EventHandler<KeyEvent> {
         quitButton.setTranslateX(750);
         quitButton.setTranslateY(700);
         quitButton.setMinSize(400,100);
-        quitButton.setStyle("-fx-font-size: 4em; ");
         // event handler for when its clicked and changes the color
         quitButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
@@ -107,37 +103,28 @@ public class Maze extends Application implements EventHandler<KeyEvent> {
         scoreButton.setFocusTraversable(false);
         quitButton.setFocusTraversable(false);
 
+
+
         // adds all the button to root so it shows up
         root.getChildren().addAll(playButton, scoreButton, quitButton, title);
 
         return mainMenuScene;
     }
 
-    public Scene playGameScene(){
-        Group root2;
-        root2 = new Group();
-        Scene playGameScene = new Scene(root2,1920, 1080, Color.GREY);
+    public Scene askSizeScene(){
+        Group root4;
+        root4 = new Group();
+        Scene askSizeScene = new Scene(root4,1920, 1080, Color.web("#272932"));
+        askSizeScene.getStylesheets().add("Styles.css");
 
-        playGameScene.setOnKeyPressed(this);
+        title.setX(525);
 
-        Text title = new Text(900,100,"Maze Game");
-        title.setFont(Font.font("Arial", FontWeight.BOLD, FontPosture.ITALIC, 80));
-        title.setFill(WHITE);
-        title.setX(725);
-        title.setY(100);
-
-
-        Text levelTitle = new Text(900,100,"Level 1");
-        levelTitle.setFont(Font.font("Arial", FontWeight.BOLD, 50));
-        levelTitle.setFill(WHITE);
-        levelTitle.setX(850);
-        levelTitle.setY(200);
 
         Button mainMenuButton = new Button("Main Menu");
         mainMenuButton.setTranslateX(100);
         mainMenuButton.setTranslateY(50);
         mainMenuButton.setMinSize(200,50);
-        mainMenuButton.setStyle("-fx-font-size: 2em; ");
+        mainMenuButton.setId("mainMenuButton");
         // event handler for when its clicked and changes the color
         mainMenuButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
@@ -151,7 +138,112 @@ public class Maze extends Application implements EventHandler<KeyEvent> {
         quitButton.setTranslateX(1620);
         quitButton.setTranslateY(50);
         quitButton.setMinSize(200,50);
-        quitButton.setStyle("-fx-font-size: 2em; ");
+        quitButton.setId("quitButton");
+        // event handler for when its clicked and changes the color
+        quitButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                System.exit(1);
+            }
+        });
+
+        Button easyButton = new Button("Easy");
+        easyButton.setTranslateX(750);
+        easyButton.setTranslateY(300);
+        easyButton.setMinSize(400,100);
+        easyButton.setId("easyButton");
+        // event handler for when its clicked and changes the color
+        easyButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                size = 15;
+                window.setScene(playGameScene());
+            }
+        });
+
+        Button mediumButton = new Button("Medium");
+        mediumButton.setTranslateX(750);
+        mediumButton.setTranslateY(500);
+        mediumButton.setMinSize(400,100);
+        mediumButton.setId("mediumButton");
+        // event handler for when its clicked and changes the color
+        mediumButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                size = 25;
+                window.setScene(playGameScene());
+            }
+        });
+
+        Button hardButton = new Button("Hard");
+        hardButton.setTranslateX(750);
+        hardButton.setTranslateY(700);
+        hardButton.setMinSize(400,100);
+        hardButton.setId("hardButton");
+        // event handler for when its clicked and changes the color
+        hardButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                size = 35;
+                window.setScene(playGameScene());
+            }
+        });
+
+
+
+        mainMenuButton.setFocusTraversable(false);
+        quitButton.setFocusTraversable(false);
+        easyButton.setFocusTraversable(false);
+        mediumButton.setFocusTraversable(false);
+        hardButton.setFocusTraversable(false);
+
+        root4.getChildren().addAll(mainMenuButton,quitButton,title, easyButton, mediumButton, hardButton);
+
+        return askSizeScene;
+    }
+
+
+
+    public Scene playGameScene(){
+        Group root2;
+        root2 = new Group();
+        Scene playGameScene = new Scene(root2,1920, 1080, Color.web("#272932"));
+        playGameScene.getStylesheets().add("Styles.css");
+
+
+        playGameScene.setOnKeyPressed(this);
+
+
+        title.setX(525);
+
+
+        Text movesTitle = new Text(1475,300,"Moves: ");
+        movesTitle.setFont(Font.font("Arial", FontWeight.BOLD, 50));
+        movesTitle.setFill(WHITE);
+
+        moveCounterTitle.setFont(Font.font("Arial", FontWeight.BOLD, 50));
+        moveCounterTitle.setFill(WHITE);
+
+
+        Button mainMenuButton = new Button("Main Menu");
+        mainMenuButton.setTranslateX(100);
+        mainMenuButton.setTranslateY(50);
+        mainMenuButton.setMinSize(200,50);
+        mainMenuButton.setId("mainMenuButton");
+        // event handler for when its clicked and changes the color
+        mainMenuButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                window.setScene(mainMenu());
+            }
+        });
+
+
+        Button quitButton = new Button("Quit");
+        quitButton.setTranslateX(1620);
+        quitButton.setTranslateY(50);
+        quitButton.setMinSize(200,50);
+        quitButton.setId("quitButton");
         // event handler for when its clicked and changes the color
         quitButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
@@ -235,13 +327,7 @@ public class Maze extends Application implements EventHandler<KeyEvent> {
 */
 
 
-
-
-
-
-
-
-        root2.getChildren().addAll(mainMenuButton,quitButton,title,levelTitle, recPlayer);
+        root2.getChildren().addAll(mainMenuButton,quitButton,title,movesTitle, moveCounterTitle, recPlayer);
 
         return playGameScene;
     }
@@ -249,26 +335,22 @@ public class Maze extends Application implements EventHandler<KeyEvent> {
     public Scene scoreScene(){
         Group root3;
         root3 = new Group();
-        Scene scoreScene = new Scene(root3,1920, 1080, Color.GREY);
+        Scene scoreScene = new Scene(root3,1920, 1080, Color.web("#272932"));
+        scoreScene.getStylesheets().add("Styles.css");
 
-        Text title = new Text(900,100,"Maze Game");
-        title.setFont(Font.font("Arial", FontWeight.BOLD, FontPosture.ITALIC, 80));
-        title.setFill(WHITE);
-        title.setX(725);
-        title.setY(100);
 
-        Text scoreTitle = new Text(900,100,"High Scores");
+        title.setX(525);
+
+        Text scoreTitle = new Text(800,200,"High Scores");
         scoreTitle.setFont(Font.font("Arial", FontWeight.BOLD, 50));
         scoreTitle.setFill(WHITE);
-        scoreTitle.setX(800);
-        scoreTitle.setY(200);
 
 
         Button mainMenuButton = new Button("Main Menu");
         mainMenuButton.setTranslateX(100);
         mainMenuButton.setTranslateY(50);
         mainMenuButton.setMinSize(200,50);
-        mainMenuButton.setStyle("-fx-font-size: 2em; ");
+        mainMenuButton.setId("mainMenuButton");
         // event handler for when its clicked and changes the color
         mainMenuButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
@@ -282,7 +364,7 @@ public class Maze extends Application implements EventHandler<KeyEvent> {
         quitButton.setTranslateX(1620);
         quitButton.setTranslateY(50);
         quitButton.setMinSize(200,50);
-        quitButton.setStyle("-fx-font-size: 2em; ");
+        quitButton.setId("quitButton");
         // event handler for when its clicked and changes the color
         quitButton.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
@@ -311,21 +393,30 @@ public class Maze extends Application implements EventHandler<KeyEvent> {
             playerYpos += 20;
             recPlayer.setY(playerYpos);
             playRow++;
+            moveCount++;
+            moveCounterTitle.setText(""+moveCount);
+
         }
         if(event.getCode() == KeyCode.UP && !maze.getMaze()[playRow][playCol].isUpWall()){
             playerYpos -= 20;
             recPlayer.setY(playerYpos);
             playRow--;
+            moveCount++;
+            moveCounterTitle.setText(""+moveCount);
         }
         if(event.getCode() == KeyCode.LEFT && !maze.getMaze()[playRow][playCol].isLeftWall()){
             playerXpos -= 20;
             recPlayer.setX(playerXpos);
             playCol--;
+            moveCount++;
+            moveCounterTitle.setText(""+moveCount);
         }
         if(event.getCode() == KeyCode.RIGHT && !maze.getMaze()[playRow][playCol].isRightWall()){
             playerXpos += 20;
             recPlayer.setX(playerXpos);
             playCol++;
+            moveCount++;
+            moveCounterTitle.setText(""+moveCount);
         }
     }
 }
--- src/MazeGame.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-public class MazeGame {
-    public static void main(String[] args) {
-
-    }
-
-
-}
